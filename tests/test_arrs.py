79889698ef9231bf2bdd1a84f81d035023da90e7
burnaschev <informal201144@gmail.com>
2023-05-22 22:36:47 +0400

    fix bag test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -11,4 +11,4 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
     assert arrs.my_slice([]) == []
     assert arrs.my_slice([1, 2, 3], -1, 2) == []
-    assert arrs.my_slice([-1, -2, -3], -5, 1) == []
+    assert arrs.my_slice([-1, -2, -3], -5, 1) == [-1]
